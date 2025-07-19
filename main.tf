33317df5e904a6f4d0e8cc369c53171f671e8bec
kevinczkm <kevinc.zkm@gmail.com>
2025-07-19 11:01:06 +0800

    added yaml file and other tf files

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# Security Group
+resource "aws_security_group" "app_sg" {
+  name        = "${local.prefix}-sg"
+  description = "Allow HTTP access"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
