5d5d65627cfa89b8804e0c4bf73b077c15bdba95
kevinczkm <kevinc.zkm@gmail.com>
2025-07-19 12:29:57 +0800

    added backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "kvin"  #Change this
+    region = "ap-southeast-1"
+  }
+}
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix =  "kvin-s3bucket" # Set your bucket name here
+}
