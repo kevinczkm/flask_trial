69c894245484e61f2cfc89f8ab35265b73cc0e5d
kevinczkm <kevinc.zkm@gmail.com>
2025-07-19 11:51:56 +0800

    modified main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,11 @@ locals {
   prefix = "kvin"
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+
 # Security Group
 resource "aws_security_group" "app_sg" {
   name        = "${local.prefix}-sg"
@@ -62,3 +67,26 @@ resource "aws_ecs_task_definition" "app" {
     }
   ])
 }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
