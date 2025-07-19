1f6e3de7de377c8ac7b50daac79b54982fee92b7
kevinczkm <kevinc.zkm@gmail.com>
2025-07-19 12:07:43 +0800

    added variables.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "512"
   memory                   = "1024"
   network_mode             = "awsvpc"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+#   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
