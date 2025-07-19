bedd960ad593cb45f6be80471c991e9c01c41fc5
kevinczkm <kevinc.zkm@gmail.com>
2025-07-19 11:47:56 +0800

    modified main.tf file with iam resource

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.1"
+
+  name = "${local.prefix}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
